18027ea3d769a7a8a2e98ae1115db1553c1dc47a
Daanish-Khan <dkhan045@uottawa.ca>
2023-03-29 21:30:15 -0400

    suffed sign in page lol

--- hotel-app/src/Pages/SignIn.js
+++ hotel-app/src/Pages/SignIn.js
@@ -1,10 +1,27 @@
-
+import './SignIn.css';
+import test from '../test.jpg' ;
 
 function SignIn() {
     return (
-        <div>
-            wolololo
-        </div>
+        <section>
+            <img src={test} className="bg" />
+            <div className="login">
+                <h2>Sign In</h2>
+                <div className="inputbox">
+                    <input type="text" placeholder="Username" />
+                </div>
+                <div className="inputbox">
+                    <input type="text" placeholder="Password" />
+                </div>
+                <div className="inputbox">
+                    <input type="submit" value="login" id="btn"/>
+                </div>
+                <div className="group">
+                    <a href="#">Forgot Password</a>
+                    <a href="3">Sign Up</a>
+                </div>
+            </div>
+        </section>
     );
 }
 
