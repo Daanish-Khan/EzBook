77a27f71c1132d71031f20cd2c6aab0a884a2f27
Daanish-Khan <dkhan045@uottawa.ca>
2023-03-30 20:00:33 -0400

    more stlying

--- hotel-app/src/App.js
+++ hotel-app/src/App.js
@@ -1,13 +1,24 @@
 import './App.css';
 import SignIn from './Pages/SignIn';
 import { Routes, Route } from 'react-router-dom';
+import { createTheme } from '@mui/material/styles';
+import { ThemeProvider } from '@emotion/react';
 
+const theme = createTheme({
+  typography: {
+    allVariants: {
+      fontFamily: 'Poppins',
+    }
+  }
+});
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<SignIn />} />
-    </Routes>
+    <ThemeProvider theme={theme}>
+      <Routes>
+        <Route path="/" element={<SignIn />} />
+      </Routes>
+    </ThemeProvider>
   
   );
 }
