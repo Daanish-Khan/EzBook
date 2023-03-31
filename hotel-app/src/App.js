80af2cb545e78d3d7bfefe937d5aca4b6e1edf51
JasonFGC <JasonLFGC@gmail.com>
2023-03-30 23:51:52 -0400

    stack not functioning dish take look smile

--- hotel-app/src/App.js
+++ hotel-app/src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
-import SignIn from './Pages/SignIn';
+// import SignIn from './Pages/SignIn';
+import CustomerBookings from './Pages/customerBookings';
 import { Routes, Route } from 'react-router-dom';
 
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<SignIn />} />
+      <Route path="/" element={<CustomerBookings />} />
     </Routes>
   
   );
