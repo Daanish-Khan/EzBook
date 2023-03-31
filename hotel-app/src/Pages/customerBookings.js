32aa25ca98127b6091dda51762d394029789f3bf
JasonFGC <JasonLFGC@gmail.com>
2023-03-31 15:41:31 -0400

    added background and list

--- hotel-app/src/Pages/customerBookings.js
+++ hotel-app/src/Pages/customerBookings.js
@@ -1,74 +1,101 @@
 import { Stack } from '@mui/system';
+import './customerBookings.css';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
 import { COLORS } from '../components/consts'
 import Chip from '@mui/material/Chip';
 import Navbar from './navbar';
-import { colors } from '@mui/material';
+import { FixedSizeList } from 'react-window';
+
+function renderRow(props) {
+    const { index, style } = props;
+
+    return (
+        <ListItem style={style} key={index} component="div" disablePadding>
+            <ListItemButton>
+                <ListItemText primary={`Booking ${index + 1}`}
+                style={{
+                    color:'white'
+
+                }}>
+                </ListItemText>
+            </ListItemButton>
+        </ListItem>
+    );
+}
 export default function customerBookings() {
     return (
 
 
-        <div
+        <div className='bg'
             style={{
-            margin: 0,
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center"
-        }}>
-            <Navbar/>
-            <Stack spacing={2} direction="column">
+                display: "flex",
+                justifyContent: "center",
+                marginTop: 50
+            }}>
+            <Navbar />
 
             <Stack spacing={2} direction="column"
                 style={{
-                    margin:50
+                    margin: 50
                 }}>
                 <Stack spacing={5} direction="row">
-                    <Chip label="Start/End"  
+                    <Chip label="Start/End"
                         sx={{
-                        background:COLORS.defaultColor,
-                        color:'white'
+                            background: COLORS.defaultColor,
+                            color: 'white'
                         }}
                     />
-                    <Chip label="Room Capacity"  
+                    <Chip label="Room Capacity"
                         sx={{
-                            background:COLORS.defaultColor,
-                            color:'white'
-                            }}
+                            background: COLORS.defaultColor,
+                            color: 'white'
+                        }}
                     />
-                    <Chip label="Area"  
+                    <Chip label="Area"
                         sx={{
-                            background:COLORS.defaultColor,
-                            color:'white'
-                            }}
+                            background: COLORS.defaultColor,
+                            color: 'white'
+                        }}
                     />
-                    <Chip label="Chain"  
+                    <Chip label="Chain"
                         sx={{
-                            background:COLORS.defaultColor,
-                            color:'white'
-                            }}
+                            background: COLORS.defaultColor,
+                            color: 'white'
+                        }}
                     />
-                    <Chip label="Category"  
+                    <Chip label="Category"
                         sx={{
-                            background:COLORS.defaultColor,
-                            color:'white'
-                            }}
+                            background: COLORS.defaultColor,
+                            color: 'white'
+                        }}
                     />
-                    <Chip label="# of Rooms"  
+                    <Chip label="# of Rooms"
                         sx={{
-                            background:COLORS.defaultColor,
-                            color:'white'
-                            }}
+                            background: COLORS.defaultColor,
+                            color: 'white'
+                        }}
                     />
 
-                    <Chip label="Price"  
+                    <Chip label="Price"
                         sx={{
-                            background:COLORS.defaultColor,
-                            color:'white'
-                            }}
+                            background: COLORS.defaultColor,
+                            color: 'white'
+                        }}
                     />
                 </Stack>
+                <FixedSizeList
+                    height={400}
+                    width={800}
+                    itemSize={46}
+                    itemCount={10}
+                    overscanCount={5}
+                >
+                {renderRow}
+                </FixedSizeList>
             </Stack>
-            </Stack>
-            
+
         </div>
     );
-  }
+}
