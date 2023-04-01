49907e585adde318342a84ccee3b6d6a6f8df450
Daanish-Khan <dkhan045@uottawa.ca>
2023-04-01 14:31:54 -0400

    db api

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,19 @@
   import express from "express"
+  import mysql from "mysql"
 
   const app = express()
+  const db = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"password",
+    database:"hotel_project_db"
+  })
+
+  app.get("/", (req, res) => {
+    res.json("wheeeee")
+  })
+
+  app.get("/hotels")
 
   app.listen(8800, () => {
     console.log("Connected to db!")
