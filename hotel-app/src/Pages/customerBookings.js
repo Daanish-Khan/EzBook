d0f5873e0810cfddac63dfa35a662a6843e038cc
Daanish-Khan <dkhan045@uottawa.ca>
2023-04-01 21:58:29 -0400

    adjust chips and layout

--- hotel-app/src/Pages/customerBookings.js
+++ hotel-app/src/Pages/customerBookings.js
@@ -4,9 +4,19 @@ import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
 import { COLORS } from '../components/consts'
-import { Chip, Grid, Box } from '@mui/material';
+import { Chip, Stack, Box } from '@mui/material';
 import Navbar from '../components/navbar';
 import { FixedSizeList } from 'react-window';
+import Waves from '../components/Waves';
+
+const chips = [
+    {key: 0, label: 'Start - End Date'},
+    {key: 1, label: 'Area'},
+    {key: 2, label: 'Chain'},
+    {key: 3, label: 'Category'},
+    {key: 4, label: '# of Rooms'},
+    {key: 5, label: 'Price'}
+];
 
 function renderRow(props) {
     const { index, style } = props;
@@ -32,99 +42,95 @@ export default function customerBookings() {
             display: "flex",
             width: "100vw",
             height: "100vh",
-            padding: 0,
-            margin: 0,
+            alignItems: "center",
+            justifyContent: "center",
         }}>
             <div className='bg' />
-            <Navbar sx={{
-                borderBottomLeftRadius: "20px", 
-                borderBottomRightRadius: "20px", 
-                backgroundColor: COLORS.defaultColor,
-                top: "0",
-                left: "0",
-                position: "absolute",
-            }}/>
-            <Box
-                sx={{
+
+            <Waves
+                    colorArray={[
+                        "#FA7268",
+                        "#EF5F67",
+                        "#E34C67",
+                        "#D53867",
+                        "#C62368",
+                    ]}
+                    style={{
+                        position:"absolute",
+                        width: "100vw",
+                        height: "55vh",
+                        bottom:"0px",
+                        left:"0px",
+                        margin: "0px"
+                    }}
+                    gap={100}
+                    height={500}
+                    speed={0.35}
+                    points={5}
+                    amplitude={20}
+                />
+
+            
+            <Stack useFlexGap sx={{width: "100vw", height: "100vh"}} spacing={0} >
+                <Navbar sx={{
+                    borderBottomLeftRadius: "20px", 
+                    borderBottomRightRadius: "20px", 
+                    backgroundColor: COLORS.defaultColor,
+                    top: "0",
+                    left: "0",
+                    padding: 0,
+                    margin: 0,
                     position: "relative",
-                    padding: "60px",
-                    paddingTop: "20px",
-                    marginTop: "90px",
-                    marginBottom: "25px",
-                    marginX: "20px",
-                    backgroundColor: "#fffff40",
-                    backdropFilter: "blur(15px)",
-                    border: "1px solid #fff",
-                    borderBottom: "1px solid #ffffff80",
-                    borderRight: "1px solid #ffffff80",
-                    borderRadius: "20px",
-                    width: "100vw",
-                    boxShadow: "0 25px 50px #0000001a",
-                }}>
-                    <Grid container spacing={1} 
-                        sx={{
-                            width: "50%"
-                        }}
-                    >
-                        <Grid item xs={1}>
-                            <Chip label="Start/End"
+                }}/>
+                
+                <Box
+                    sx={{
+                        position: "relative",
+                        marginX: "100px",
+                        marginTop: "50px"
+                    }}
+                >
+                    {chips.map((data) => {
+                        return (
+                            <Chip 
+                                label={data.label}
                                 sx={{
-                                    background: COLORS.defaultColor,
-                                    color: 'white'
+                                    color: "white",
+                                    backgroundColor: COLORS.defaultColor,
+                                    margin: "10px"
                                 }}
                             />
-                        </Grid>
-                        <Grid item xs>
-                            <Chip label="Area"
-                                sx={{
-                                    background: COLORS.defaultColor,
-                                    color: 'white'
-                                }}
-                            />
-                        </Grid>
-                        <Grid item xs>
-                            <Chip label="Chain"
-                                sx={{
-                                    background: COLORS.defaultColor,
-                                    color: 'white'
-                                }}
-                            />
-                        </Grid>
-                        <Grid item xs>
-                            <Chip label="Category"
-                                sx={{
-                                    background: COLORS.defaultColor,
-                                    color: 'white'
-                                }}
-                            />
-                        </Grid>
-                        <Grid item xs>
-                            <Chip label="# of Rooms"
-                                sx={{
-                                    background: COLORS.defaultColor,
-                                    color: 'white'
-                                }}
-                            />
-                        </Grid>
-                        <Grid item xs>
-                            <Chip label="Price"
-                                sx={{
-                                    background: COLORS.defaultColor,
-                                    color: 'white'
-                                }}
-                            />
-                        </Grid>
-                    </Grid>
-                    <FixedSizeList
-                                    height={400}
-                                    width={800}
-                                    itemSize={46}
-                                    itemCount={10}
-                                    overscanCount={5}
-                            >
-                                {renderRow}
-                            </FixedSizeList>  
+                        );
+                    })}
                 </Box>
+                <Box
+                    sx={{
+                        position: "relative",
+                        padding: "60px",
+                        paddingTop: "20px",
+                        marginTop: "10px",
+                        marginX: "100px",
+                        backgroundColor: "#fffff40",
+                        backdropFilter: "blur(15px)",
+                        border: "1px solid #fff",
+                        borderBottom: "1px solid #ffffff80",
+                        borderRight: "1px solid #ffffff80",
+                        borderRadius: "20px",
+                        height: "75vh",
+                        boxShadow: "0 25px 50px #0000001a",
+                    }}>
+                    <FixedSizeList
+                        height={400}
+                        width={800}
+                        itemSize={46}
+                        itemCount={10}
+                        overscanCount={5}
+                    >
+                        {renderRow}
+                    </FixedSizeList>  
+                </Box>
+            </Stack>
+            
         </Container>
         
     );
