74363f1a4b6bc1e05708e40d4feb2cc71c528ed6
Daanish-Khan <dkhan045@uottawa.ca>
2023-04-07 16:48:04 -0400

    cosntructing query for search

--- backend/index.js
+++ backend/index.js
@@ -9,11 +9,85 @@
     database:"hotel_project_db"
   })
 
-  app.get("/", (req, res) => {
-    res.json("wheeeee")
+  app.use(express.json())
+
+  /**
+   * EXAMPLE QUERY, search by date, area, and rating 
+   * 
+   * {
+   * 
+   *  start_date: "2020-02-02",
+   *  end_date: "2020-02-05",
+   *  room_capacity: "",
+   *  city: Santa Clara,
+   *  country: "Speedshead Bay",
+   *  chain: "",
+   *  rating: 3,
+   *  num_rooms: "",
+   *  price_low: "",
+   *  price_high: ""
+   * 
+   * }
+   * 
+   * 
+   */
+
+  app.post("/query", (req, res) => {
+    const q_start = "SELECT r.*, h.chainName, h.city, h.country, h.star_rating, h.num_rooms " +
+                    "FROM rooms r " +
+                    "JOIN hotels h ON r.hotel = h.address "
+    let q_middle = ""
+    const q_end = ";"
+
+    // Start/end date
+    if (req.body.start_date !== "" || req.body.end_date !== "") {
+      q_middle += "LEFT JOIN bookings b ON r.room_num = b.room_num AND r.hotel = b.hotel " +
+                  "WHERE b.room_num IS NULL OR (";
+      if (req.body.start_date !== "") {
+        q_middle += `b.endDate < '${req.body.start_date}' `
+
+        if (req.body.end_date !== "") {
+          q_middle += `OR b.startDate > '${req.body.end_date}'`
+        }
+        
+        q_middle += ")"
+
+      } else if (req.body.end_date !== "") {
+        q_middle += `b.startDate > '${req.body.end_date}')`
+      }
+
+    } 
+
+    // Capacity
+    if (req.body.room_capacity !== "") {
+      q_middle += ` AND r.capacity >= '${req.body.room_capacity}'`
+    }
+
+    const q = q_start + q_middle + q_end;
+
+    console.log(q);
+
+    db.query(q, (err, data) => {
+      if (err) {
+        return res.json(err)
+      }
+
+      return res.json(data)
+    })
   })
 
-  app.get("/hotels")
+  app.post("/books", (req, res) => {
+    const q = "INSERT INTO rooms (`room_num`, `hotel`, `price`, `capacity`, `view_type`, `amenities`, `expandable`, `status`) VALUES (?)"
+    const values = [6, "112 Flint Place", 0.00, 1, null, "test", 0, "Available"]
+
+    db.query(q, [values], (err, data) => {
+      if (err) {
+        return res.json(err)
+      }
+
+      return res.json(data)
+    })
+  })
 
   app.listen(8800, () => {
     console.log("Connected to db!")
